51c1a8893310626bf695a2c3418e419073f4780e
Geoffrey <geo.galagaran@gmail.com>
2017-07-02 12:04:53 +0800

    Remade tableSerialize and createTable functionality Add createTables method

--- model/model.js
+++ model/model.js
@@ -1,41 +1,47 @@
 const mysql = require('mysql')
 
+
 class DBConnect {
   constructor(settings) {
     const { host, user, password, database } = settings
-    this.test = 'wew'
     this.connection = mysql.createConnection({ host, user, password, database })
+    this.createTable = this.createTable.bind(this)
+    this.createTables = this.createTables.bind(this)
   }
 
-  tableSerialize({ table_name, auto_id, columns }) {
-    const tableColumns = columns.map(column => `${column.name.toLowerCase()} ${column.type.toUpperCase()}(${column.size})`)
-    const id = auto_id ? 'id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, ' : ''
-    return `CREATE TABLE ${table_name} (${id}${tableColumns.join(', ')});`
-  }
+  tableSerialize({ table_name, primary_key, foreign_key, columns }) {
+    let id = '', foreignId = ''
 
-  wrapConnection(connection, callback) {
-    connection.connect()
-    callback()
-    connection.end()
+    if(primary_key) {
+      id = `${primary_key} INT PRIMARY KEY AUTO_INCREMENT NOT NULL, `
+    }
+
+    if(foreign_key) {
+      const { table_ref, table_ref_key } = foreign_key
+      foreignId = `${table_ref_key}_FK INT, FOREIGN KEY (${table_ref_key}_FK) REFERENCES ${table_ref}(${table_ref_key}), `
+    }
+
+    const tableColumns = columns.map(column => {
+      const name = column.name.toLowerCase()
+      const type = column.type.toUpperCase()
+      const size = column.size ? `(${column.size})` : ''
+      return `${name} ${type}${size}`
+    }).join(', ')
+
+
+    return `CREATE TABLE ${table_name} (${id}${foreignId}${tableColumns});`
   }
 
   createTable(data) {
     const { connection, wrapConnection, tableSerialize } = this
-    wrapConnection(connection, () => connection.query(tableSerialize(data)))
+    connection.query(tableSerialize(data))
   }
 
   createTables(data_array) {
+    const { createTable } = this
     return data_array.map(data => createTable(data))
   }
 }
 
-initAppTables = (model) => {
-  model.createTables([
-    {
 
-    },
-
-  ])
-}
-
-module.exports = { DBConnect, initAppTables }
+module.exports = { DBConnect }
